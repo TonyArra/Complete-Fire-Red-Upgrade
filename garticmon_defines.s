82cc4c847bfba782fb97d710793f7f0021d06e17
Tony Arra <ttonyarra@gmail.com>
2022-01-14 12:06:21 -0500

    Sort by hex codes

--- garticmon_defines.s
+++ garticmon_defines.s
@@ -1,26 +1,26 @@
 @ Vars
 @ See: /includes/constants/vars.h
 .equ VAR_MAP_SCENE_PALLET_TOWN_OAK, 0x4050
-.equ VAR_MAP_SCENE_PALLET_TOWN_PLAYERS_HOUSE_2F, 0x4056
 .equ VAR_MAP_SCENE_PALLET_TOWN_PROFESSOR_OAKS_LAB, 0x4055
+.equ VAR_MAP_SCENE_PALLET_TOWN_PLAYERS_HOUSE_2F, 0x4056
 
 @ Flags
 @ See: /includes/constants/flags.h
-.equ FLAG_DONT_TRANSITION_MUSIC, 0x4001
 .equ FLAG_HIDE_RIVAL_IN_PLAYER_ROOM, 0x900
 .equ FLAG_HIDE_ROUTE1_TOKEN_GRUNT, 0x901
 .equ FLAG_HIDE_OAK_IN_HIS_LAB, 0x2B
 .equ FLAG_HIDE_OAK_IN_PALLET_TOWN, 0x2C
+.equ FLAG_DONT_TRANSITION_MUSIC, 0x4001
 
 @ Songs
 @ See: https://www.pokecommunity.com/showpost.php?p=9705692&postcount=10
-.equ SONG_GAME_CORNER_1, 0x146
 .equ SONG_PALLET_TOWN, 0x12C
 .equ SONG_OAKS_THEME, 0x12E
+.equ SONG_GAME_CORNER_1, 0x146
 
 @ Sounds
 @ See: https://www.pokecommunity.com/showpost.php?p=9705692&postcount=10
+.equ SOUND_JUMP, 0xA
 .equ SOUND_FLEE, 0x11
 .equ SOUND_HIGH_PITCH_BEEP, 0x15
 .equ SOUND_INSERT_COIN, 0x19
-.equ SOUND_JUMP, 0xA
