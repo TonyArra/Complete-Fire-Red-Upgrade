bab95cc4d3201904b9f5a77ee5277860f5d3a612
Tony Arra <ttonyarra@gmail.com>
2022-01-23 13:28:20 -0500

    Add sleeping player animations to player's room scene (#10)
    * Add sleeping player animations to player's room scene
    
    Add XSE
    
    * Run down stairs

--- assembly/overworld_scripts/pallet_town/player_room.s
+++ assembly/overworld_scripts/pallet_town/player_room.s
@@ -4,15 +4,37 @@
 .include "../xse_defines.s"
 .include "../garticmon_defines.s"
 
-@ Map script that runs when starting a new game in the player room
+@; Map script that runs when starting a new game in the player room.
 
-@ Person Events
 .equ PERSON_RIVAL, 0x1
+.equ PERSON_SLEEPING_PLAYER, 0x2
 
 .global gMapScripts_PlayerRoom_Start
 gMapScripts_PlayerRoom_Start:
+  mapscript MAP_SCRIPT_ON_LOAD PlayerRoom_OnLoad
   mapscript MAP_SCRIPT_ON_FRAME_TABLE PlayerRoom_OnFrameTable
   .byte MAP_SCRIPT_TERMIN
+
+PlayerRoom_OnLoad:
+  compare VAR_MAP_SCENE_PALLET_TOWN_PLAYERS_HOUSE_2F 0x0
+  call_if equal RenderSleepingSprite
+  end
+
+RenderSleepingSprite:
+  checkgender
+  compare LASTRESULT BOY
+  call_if equal RenderMaleSleepingSprite
+  compare LASTRESULT GIRL
+  call_if equal RenderFemaleSleepingSprite
+  return
+
+RenderMaleSleepingSprite:
+  setvar VAR_RUNTIME_CHANGEABLE MALE_SLEEPING_SPRITE
+  return
+
+RenderFemaleSleepingSprite:
+  setvar VAR_RUNTIME_CHANGEABLE FEMALE_SLEEPING_SPRITE
+  return
 
 PlayerRoom_OnFrameTable:
   levelscript VAR_MAP_SCENE_PALLET_TOWN_PLAYERS_HOUSE_2F 0x0 PlayerRoom_EventScript_MeetRival
@@ -20,17 +42,29 @@ PlayerRoom_OnFrameTable:
 
 PlayerRoom_EventScript_MeetRival:
   setvar VAR_MAP_SCENE_PALLET_TOWN_PLAYERS_HOUSE_2F 0x1
+  call SetHealingPlaceToHome
+  call Player_Sleep
   msgbox gText_PlayerRoom_Rival_WakeUp MSG_NORMAL
+  call Player_WakeUp
   playsong SONG_GAME_CORNER_1
   call Rival_Enters
   msgbox gText_PlayerRoom_Rival_Late MSG_NORMAL
   call Rival_Exits
   fadesong SONG_PALLET_TOWN
-  msgbox gText_PlayerRoom_Player_Festival1 MSG_NORMAL
-  msgbox gText_PlayerRoom_Player_Festival2 MSG_NORMAL
-  setflag FLAG_HIDE_RIVAL_IN_PLAYER_ROOM
   release
   end
+
+Player_Sleep:
+  applymovement PLAYER Move_Player_Hide
+  msgbox gText_PlayerRoom_Player_Snore MSG_NORMAL
+  pause 0x3C
+  msgbox gText_PlayerRoom_Player_Snore MSG_NORMAL
+  pause 0x3C
+  return
+
+Move_Player_Hide:
+  .byte set_invisible
+  .byte end_m
 
 Rival_Enters:
   applymovement PERSON_RIVAL Move_Rival_Enters
@@ -70,21 +104,43 @@ Move_Rival_Exclaim:
   .byte say_double_exclaim
   .byte end_m
 
+Player_WakeUp:
+  sound SOUND_HIGH_PITCH_BEEP
+  applymovement PLAYER Move_Player_WakeUp
+  waitmovement 0x0
+  hidesprite PERSON_SLEEPING_PLAYER
+  return
+
+Move_Player_WakeUp:
+  .byte set_visible
+  .byte exclaim
+  .byte end_m
+
 Rival_Exits:
   applymovement PERSON_RIVAL Move_Rival_Exits
   waitmovement 0x0
+  sound SOUND_EXIT_ROOM
+  hidesprite PERSON_RIVAL
   return
 
 Move_Rival_Exits:
+  .byte run_up
+  .byte run_up
+  .byte run_right
+  .byte run_right
+  .byte run_right
+  .byte run_right
   .byte run_down
   .byte run_right
   .byte run_right
   .byte run_right
   .byte run_right
-  .byte run_right
-  .byte run_right
-  .byte run_right
-  .byte run_right
-  .byte run_right
-  .byte run_right
+  .byte run_up
+  .byte run_up
+  .byte walk_left
+  .byte walk_left
   .byte end_m
+
+SetHealingPlaceToHome:
+  sethealingplace 0x1
+  return
